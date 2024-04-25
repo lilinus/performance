8788b69d993716a009579fe11ce861de14956449
lilinus <linus.hamlin@outlook.com>
2024-04-25 16:49:34 +0200

    Guid.CompareTo(object)

---
+++ src/benchmarks/micro/libraries/System.Runtime/Perf.TestPr.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using Newtonsoft.Json.Linq;
+
+namespace System.Tests
+{
+	[BenchmarkCategory(Categories.Libraries)]
+	public class Perf_TestPr
+	{
+		public static Guid GuidVal = Guid.NewGuid();
+		[Benchmark]
+		public int CompareTo_Guid() => GuidVal.CompareTo((object)GuidVal);
+	}
+}
