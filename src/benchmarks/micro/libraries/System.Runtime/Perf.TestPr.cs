74b632eec20dedd5fcd759a47a8b07ea3f0ab228
lilinus <linus.hamlin@outlook.com>
2024-05-13 09:33:56 +0200

    Simplify TimeSpanFormat benchmarks

--- src/benchmarks/micro/libraries/System.Runtime/Perf.TestPr.cs
+++ src/benchmarks/micro/libraries/System.Runtime/Perf.TestPr.cs
@@ -16,34 +16,18 @@ namespace System.Tests
 	{
 		public static IEnumerable<object[]> DateTimeFormatValues()
 		{
-			yield return new object[] { new DateTime(123456789U), @"yyyy\-MM\-dd\.HH\.mm\.ss\.fff" };
-			yield return new object[] { new DateTime(123456789U), @"yyyy\-MM\-dd\.HH\.mm\.ss\.FFFFF" };
+			yield return new object[] { new DateTime(12345678900L), @"yyyy\-MM\-dd\.HH\.mm\.ss\.FFFFFFF" };
 		}
 		[Benchmark, ArgumentsSource(nameof(DateTimeFormatValues))]
-		public string DateTime_Format(DateTime arg1, string arg2) => arg1.ToString(arg2, CultureInfo.InvariantCulture);
+		public string DateTime_Format(DateTime value, string fmt) => value.ToString(fmt, CultureInfo.InvariantCulture);
 
 		public static IEnumerable<object[]> TimeSpanFormatValues()
 		{
-			yield return new object[] { new TimeSpan(123456789U), @"d\.hh\.mm\.ss\.fffffff" };
-			yield return new object[] { new TimeSpan(123456789U), @"d\.hh\.mm\.ss\.FFFFFFF" };
+			yield return new object[] { new TimeSpan(12345678900L), @"d\.hh\.mm\.ss\.FFFFFFF" };
+			yield return new object[] { new TimeSpan(12345678900L), "g" };
+			yield return new object[] { new TimeSpan(12345678900L), "G" };
 		}
 		[Benchmark, ArgumentsSource(nameof(TimeSpanFormatValues))]
-		public string TimeSpan_Format(TimeSpan arg1, string arg2) => arg1.ToString(arg2, CultureInfo.InvariantCulture);
-
-		public static IEnumerable<object[]> DateTimeParseValues()
-		{
-			yield return new object[] { "2024-05-02.21.40.42.12345", @"yyyy\-MM\-dd\.HH\.mm\.ss\.fffff" };
-			yield return new object[] { "2024-05-02.21.40.42.123", @"yyyy\-MM\-dd\.HH\.mm\.ss\.FFFFF" };
-		}
-		[Benchmark, ArgumentsSource(nameof(DateTimeParseValues))]
-		public DateTime DateTime_Parse(string arg1, string arg2) => DateTime.ParseExact(arg1, arg2, CultureInfo.InvariantCulture);
-
-		public static IEnumerable<object[]> TimeSpanParseValues()
-		{
-			yield return new object[] { "0.00.00.00.12345", @"d\.hh\.mm\.ss\.fffff" };
-			yield return new object[] { "0.00.00.00.123", @"d\.hh\.mm\.ss\.FFFFF" };
-		}
-		[Benchmark, ArgumentsSource(nameof(TimeSpanParseValues))]
-		public TimeSpan TimeSpan_Parse(string arg1, string arg2) => TimeSpan.ParseExact(arg1, arg2, CultureInfo.InvariantCulture);
+		public string TimeSpan_Format(TimeSpan value, string fmt) => value.ToString(fmt, CultureInfo.InvariantCulture);
 	}
 }
